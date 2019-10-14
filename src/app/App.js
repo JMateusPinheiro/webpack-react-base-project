f63415fbd8704e6823d0155e6d8e04253b58499d
Joao Mateus <joaopinheiro@trixlog.com>
2019-10-14 11:18:50 -0300

    Config ESLint

--- src/app/App.js
+++ src/app/App.js
@@ -4,10 +4,11 @@ import ReactDOM from "react-dom";
 const App = () => {
   return (
     <div>
-      <span> React Base Project With Webpack </span>
+      <span> React Base Project With Webpack!</span>
     </div>
   );
 };
+
 export default App;
 
 ReactDOM.render(<App />, document.getElementById("app"));
